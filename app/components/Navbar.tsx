44f5f298b9a8c7a4d1d47be1f4c7f1c30e2bae3c
Jesse <jessebeke10@gmail.com>
2024-08-25 01:53:57 +0100

    first commit for scoutr

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav className="relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7">
+        <h1>Hello</h1>
+    </nav>
+  )
+}
