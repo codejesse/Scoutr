a23bb4cbd504ce60cb6370c43264b3623f55d068
Jesse <jessebeke10@gmail.com>
2024-09-10 22:12:59 +0100

    feat: navbar responsive sheet added

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,6 +1,17 @@
 "use client";
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "./ui/button";
 
 export function MobileMenu() {
   const [isToggled, setToggled] = useState(false);
@@ -16,10 +27,26 @@ export function MobileMenu() {
   return (
     <>
       {isToggled ? (
-        <div className="bg-white h-200px w-8/12">
-          <X onClick={handleClose} />
-          drawer eyy
-        </div>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button className="bg-none border-none" variant="outline">
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Edit profile</SheetTitle>
+              <SheetDescription>
+                Make changes to your profile here. Click save when you're done.
+              </SheetDescription>
+            </SheetHeader>
+            <SheetFooter>
+              <SheetClose asChild>
+                <Button type="submit">Save changes</Button>
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
       ) : (
         <div onClick={handleClick}>
           <Menu />
