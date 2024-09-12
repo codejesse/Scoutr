8631e22bdb701b66dd43359220d426a7875c1c79
Jesse <jessebeke10@gmail.com>
2024-09-12 22:27:54 +0100

    feat: added dropdown to usernav

--- components/UserNav.tsx
+++ components/UserNav.tsx
@@ -1,17 +1,29 @@
 import { Avatar, AvatarFallback } from "./ui/avatar";
 import { Button } from "./ui/button";
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+} from "./ui/dropdown-menu";
 
 export function UserNav() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-10 w-10 rounded-full">
-          <Avatar>
+          <Avatar className="h-10 w-10">
             <AvatarFallback>JB</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p>Jesse Beke</p>
+          </div>
+        </DropdownMenuLabel>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 }
