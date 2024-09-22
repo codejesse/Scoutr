4c737bb21558a5ecd1301c9578289e5fe5a4f2eb
Jesse <jessebeke10@gmail.com>
2024-09-22 02:45:42 +0100

    feat: tiptap editor initial set-up (1)

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEditor, type Editor } from "@tiptap/react";
+import { EditorContent, useEditor, type Editor } from "@tiptap/react";
 import { Button } from "./ui/button";
 import StarterKit from "@tiptap/starter-kit";
 
@@ -20,6 +20,45 @@ export const MenuBar = ({ editor }: { editor: Editor | null }) => {
       >
         H1
       </Button>
+      <Button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        type="button"
+        variant={
+          editor.isActive("heading", { level: 2 }) ? "default" : "secondary"
+        }
+      >
+        H2
+      </Button>
+      <Button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        type="button"
+        variant={
+          editor.isActive("heading", { level: 3 }) ? "default" : "secondary"
+        }
+      >
+        H3
+      </Button>
+      <Button
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        type="button"
+        variant={editor.isActive("bold") ? "default" : "secondary"}
+      >
+        Bold
+      </Button>
+      <Button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        type="button"
+        variant={editor.isActive("italic") ? "default" : "secondary"}
+      >
+        Italic
+      </Button>
+      <Button
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        type="button"
+        variant={editor.isActive("strike") ? "default" : "secondary"}
+      >
+        Strike
+      </Button>
     </div>
   );
 };
@@ -27,11 +66,13 @@ export const MenuBar = ({ editor }: { editor: Editor | null }) => {
 export function TipTapEditor() {
   const editor = useEditor({
     extensions: [StarterKit],
+    content: "<p>Hello World</p>",
   });
 
   return (
     <div>
       <MenuBar editor={editor} />
+      <EditorContent editor={editor} />
     </div>
   );
 }
